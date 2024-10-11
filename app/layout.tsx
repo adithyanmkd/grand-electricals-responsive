c052f9a0859f389b03d41b428d83798c6b723d5d
adithyan <adithyanmkd123@gmail.com>
2024-10-11 10:21:29 +0530

    favicon icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head></head>
       <body className={`${poppins.className} bg-[#FAF9F6]`}>
         <Navbar />
         <main>{children}</main>
